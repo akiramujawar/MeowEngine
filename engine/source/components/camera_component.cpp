9d281b8bfbcb953c6479a833e2e5c98b9bdfcd34
Akira Mujawar <wulcat@icloud.com>
2026-02-02 00:03:40 +0530

    Created camera component

---
+++ engine/source/components/camera_component.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Akira Mujawar on 26/01/26.
+//
+
+#include "camera_component.hpp"
---
+++ engine/source/components/camera_component.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Akira Mujawar on 26/01/26.
+//
+
+#ifndef MEOWENGINE_CAMERA_COMPONENT_HPP
+#define MEOWENGINE_CAMERA_COMPONENT_HPP
+
+namespace MeowEngine::entity {
+    struct CameraComponent {
+    public:
+        static void Reflect();
+
+        CameraComponent();
+    };
+}
+
+
+#endif //MEOWENGINE_CAMERA_COMPONENT_HPP
